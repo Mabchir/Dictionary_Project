a652fe67e619f0c29d1f78fad8e9a2a286a29334
Mabchir <38863638+Mabchir@users.noreply.github.com>
2021-12-15 19:56:16 -0500

    adding logo of the app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
+import logo from "./Dic_logo_svg.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <header className="App-header">
+        <img src={logo} className="App_logo" alt="logo" />
+      </header>
     </div>
   );
 }
