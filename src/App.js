c1e274e536fb8a5475bae2229822174dd108280e
Mabchir <38863638+Mabchir@users.noreply.github.com>
2021-12-16 17:48:34 -0500

    adding the search engine
    - the form
    - the function interacting with user input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./Dic_logo_svg.svg";
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App_logo" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App_logo" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Mariem Bchir</footer>
+      </div>
     </div>
   );
 }
